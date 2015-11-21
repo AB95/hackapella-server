af1f202d3381c7b650f4e1a8764a3c9fcbeea945
AB95 <aboucher598@gmail.com>
2015-11-21 13:18:03 +0000

    Initial commit

---
+++ server.py
@@ -0,0 +1,16 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+address = ('10.83.3.175', 7000)
+s.bind(address)
+incoming = 0
+
+s.listen(1)
+while True:
+    connection, client_address = s.accept()
+    print "hi"
+    incoming = connection.recv(31)
+    break
+
+s.close()
+print
